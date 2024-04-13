612532b326e4094759eb93bfd81dfcada59f4a19
jmgang <jansenmarsonang@gmail.com>
2024-04-13 16:11:55 +0800

    updates

--- src/main/java/org/assistant/api/homeassistant/EntityStateChanged.java
+++ src/main/java/org/assistant/api/homeassistant/EntityStateChanged.java
@@ -7,5 +7,6 @@ public record EntityStateChanged(
         @JsonProperty("entity_id") String entityId,
         @JsonProperty("state") String state,
         @JsonProperty("attributes") Map<String, Object> attributes,
-        @JsonProperty("last_changed") String lastChanged) {
+        @JsonProperty("last_changed") String lastChanged,
+        @JsonProperty("last_updated") String lastUpdated) {
 }
--- src/main/java/org/assistant/api/homeassistant/KasaSmartStripClient.java
+++ src/main/java/org/assistant/api/homeassistant/KasaSmartStripClient.java
@@ -1,12 +1,15 @@
 package org.assistant.api.homeassistant;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import org.assistant.api.common.HttpRequestBuilder;
 import org.assistant.api.common.SimpleHttpClient;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
+
+import static org.assistant.config.ConfigLoader.getProperty;
 
 public class KasaSmartStripClient {
     private final SimpleHttpClient httpClient;
@@ -27,16 +30,37 @@ public class KasaSmartStripClient {
         sendCommand("switch/turn_off", new HomeAssistantCommand(entityId));
     }
 
+    public boolean isOn(String entityId) {
+        try {
+            var entityChanged = getEntityState(entityId);
+            System.out.println(entityChanged);
+            return true;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private EntityStateChanged getEntityState(String entityId) throws IOException {
+        HttpRequestBuilder builder = new HttpRequestBuilder(getProperty("assistant.api.homeassistant.url"))
+                .addPathSegments("api", "states", entityId)
+                .addHeader("Authorization", "Bearer " + getProperty("assistant.api.homeassistant.access-token"))
+                .addHeader("Content-Type", "application/json");
+
+        String url = builder.build();
+        Map<String, String> headers = builder.getHeaders();
+        return httpClient.sendGet(url, headers, EntityStateChanged.class);
+    }
+
     private void sendCommand(String service, HomeAssistantCommand command) throws IOException {
-        String url = homeAssistantUrl + "/api/services/" + service;
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Authorization", "Bearer " + longLivedAccessToken);
-        headers.put("Content-Type", "application/json");
+        HttpRequestBuilder builder = new HttpRequestBuilder(homeAssistantUrl)
+                .addPathSegments("api", "services", service)
+                .addHeader("Authorization", "Bearer " + longLivedAccessToken)
+                .addHeader("Content-Type", "application/json");
+        List<EntityStateChanged> changes = httpClient.sendPost(builder.build(), builder.getHeaders(), command, new TypeReference<>() {
+        });
 
-        List<EntityStateChanged> changes = httpClient.sendPost(url, headers, command, new TypeReference<>() {});
-
-        System.out.println(changes.size());
-        changes.forEach(System.out::println);
+        System.out.println("Number of entities changed: " + changes.size());
+        changes.forEach(change -> System.out.println("Entity ID: " + change.entityId() + ", State: " + change.state()));
     }
 
     public static void main(String[] args) {
